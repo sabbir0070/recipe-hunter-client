8b242295438232b7e8d4723d75dcec8c5df9c5c0
sabbir0070 <mdsharifulalamsiddique79672@gmail.com>
2023-05-04 23:56:46 +0600

    project complete

--- src/Components/Share/Footer/Footer.jsx
+++ src/Components/Share/Footer/Footer.jsx
@@ -1,12 +1,14 @@
+import { ArrowTrendingDownIcon, FaceFrownIcon, UserCircleIcon } from '@heroicons/react/24/solid';
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { BeakerIcon } from '@heroicons/react/24/solid'
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faCediSign, faCoffee } from '@fortawesome/free-solid-svg-icons'
 // import { Link } from 'react-router-dom';
 
 const Footer = () => {
-  return (
- <div className='bg-slate-900 text-white mt-10 text-center py-5   md:px-10 lg:px-10  mx-auto  xs:w-xl  sm:max-w-xl md:max-w-full lg:max-w-screen-xl '>
+    return (
+        <div className='bg-slate-900 text-white mt-10 text-center py-5   md:px-10 lg:px-10  mx-auto  xs:w-xl  sm:max-w-xl md:max-w-full lg:max-w-screen-xl '>
             <h2 className='font-bold text-5xl text-center pt-5 hover:text-base-200'>GOOD FOOD</h2>
             <ul className='flex justify-center mt-5'>
                 <li className='md:px-2 px-1 md:font-bold'>HOME</li>
@@ -17,9 +19,9 @@ const Footer = () => {
             </ul>
 
             <ul className='flex justify-center mt-5'>
-                <li className='px-2 text-2xl cursor-pointer'>
+                <li className='px-2 text-2xl text-white cursor-pointer'>
                     {/* <FaFacebook></FaFacebook> */}
-                    
+                    <UserCircleIcon className='w-20 h-20 text-white rounded-full'></UserCircleIcon>
                 </li>
                 <li className='px-2 text-2xl cursor-pointer'>
                     {/* <FaLinkedinIn></FaLinkedinIn> */}
@@ -31,21 +33,19 @@ const Footer = () => {
                     {/* <FaTwitter></FaTwitter> */}
                 </li>
             </ul>
+            <footer className='divide-y  text-gray-800'>
+                <div className='px-10 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl'>
+                </div>
+                <div className='py-6 text-sm text-center text-gray-400'>
+                    © 2023 Job Circular Inc. All rights reserved.
+                </div>
+            </footer>
         </div>
 
 
 
-//     <div>
-//     <footer className='divide-y  text-gray-800'>
-//       <div className='px-10 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl'>
 
-// </div>
-//       <div className='py-6 text-sm text-center text-gray-400'>
-//         © 2023 Job Circular Inc. All rights reserved.
-//       </div>
-//     </footer>
-//     </div>
-  );
+    );
 };
 
-export default Footer;<h3>this is footer</h3>
+export default Footer; <h3>this is footer</h3>
--- src/Components/pages/ChefCard/ChefCard.jsx
+++ src/Components/pages/ChefCard/ChefCard.jsx
@@ -24,7 +24,7 @@ const ChefCard = ({ chef }) => {
                     rounded bg-indigo-500 text-white hover:bg-indigo-700 ">
             View Recipes </button> </Link>
           <p className='text-gray-800 flex items-center text-lg font-semibold'>
-            <HandThumbUpIcon className='w-10  h-10  mr-2 xs:mr-0 text-indigo-700'></HandThumbUpIcon> <span className='text-3xl text-red-700'>{likes}</span>
+            <HandThumbUpIcon className='w-10  h-10  mr-2 lg:ml-8 text-indigo-700'></HandThumbUpIcon> <span className='text-3xl text-red-700'>{likes}</span>
           </p>
         </div>
       </div>
